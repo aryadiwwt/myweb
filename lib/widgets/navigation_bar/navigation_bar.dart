18480eea45a7ff4588427b8aa5bdd07f10ff2974
aryadiwwt <singitterus@gmail.com>
2020-02-07 17:45:06 +0700

    Navbar

--- lib/widgets/navigation_bar/navigation_bar.dart
+++ lib/widgets/navigation_bar/navigation_bar.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:myweb/widgets/navigation_bar/navbar_logo.dart';
+
+import 'navbar_item.dart';
 
 class NaviagtionBar extends StatelessWidget {
   const NaviagtionBar({Key key}) : super(key: key);
@@ -10,36 +13,19 @@ class NaviagtionBar extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          SizedBox(
-            height: 80,
-            width: 150,
-            child: Image.asset('assets/logo.png'),
-          ),
+          NavBarLogo(),
           Row(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              _NavBarItem('Explore'),
+              NavBarItem('Explore'),
               SizedBox(
                 width: 60,
               ),
-              _NavBarItem('About'),
+              NavBarItem('About'),
             ],
           ),
         ],
       ),
-    );
-  }
-}
-
-class _NavBarItem extends StatelessWidget {
-  final String title;
-  const _NavBarItem(this.title);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      title,
-      style: TextStyle(fontSize: 18),
     );
   }
 }
