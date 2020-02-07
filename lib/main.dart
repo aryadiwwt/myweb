977286614833988c7a4ad3d25c51702ccaf86267
aryadiwwt <singitterus@gmail.com>
2020-02-07 19:42:37 +0700

    Add Responsive Bulider

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var themeData = ThemeData(
+      primarySwatch: Colors.blue,
+      textTheme: Theme.of(context).textTheme.apply(fontFamily: 'gisha'),
+    );
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        textTheme: Theme.of(context).textTheme.apply(fontFamily: 'gisha'),
-      ),
+      theme: themeData,
       home: HomeView(),
     );
   }
--- lib/widgets/course_details/course_details.dart
+++ lib/widgets/course_details/course_details.dart
@@ -1,30 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class CourseDetails extends StatelessWidget {
   const CourseDetails({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 600,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'PRINTZ ANYWHERE.',
-            style: TextStyle(
-                fontWeight: FontWeight.w800, height: 0.9, fontSize: 50),
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) {
+        var textAlignment =
+            sizingInformation.deviceScreenType == DeviceScreenType.Desktop
+                ? TextAlign.left
+                : TextAlign.center;
+        double titleSize =
+            sizingInformation.deviceScreenType == DeviceScreenType.Mobile
+                ? 50
+                : 80;
+        double descriptionSize =
+            sizingInformation.deviceScreenType == DeviceScreenType.Mobile
+                ? 16
+                : 20;
+
+        return Container(
+          width: 600,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'PRINTZ ANYWHERE.',
+                style: TextStyle(
+                    fontWeight: FontWeight.w800,
+                    height: 0.9,
+                    fontSize: titleSize),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                'Kami adalah perusahaan rintisan berbasis teknologi digital yang fokus untuk industri Digital Printing, kami membantu pelaku usaha Digital Printing dalam mengembangkan bisnisnya, disisi yang lain kami membantu konsumen untuk mendapatkan pelayanan yang efektif dan efisien, bahkan tanpa harus keluar dari rumah',
+                style: TextStyle(
+                  fontSize: descriptionSize,
+                  height: 1.5,
+                ),
+                textAlign: textAlignment,
+              )
+            ],
           ),
-          SizedBox(
-            height: 30,
-          ),
-          Text(
-            'Kami adalah perusahaan rintisan berbasis teknologi digital yang fokus untuk industri Digital Printing, kami membantu pelaku usaha Digital Printing dalam mengembangkan bisnisnya, disisi yang lain kami membantu konsumen untuk mendapatkan pelayanan yang efektif dan efisien, bahkan tanpa harus keluar dari rumah',
-            style: TextStyle(fontSize: 20, height: 1.5),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
